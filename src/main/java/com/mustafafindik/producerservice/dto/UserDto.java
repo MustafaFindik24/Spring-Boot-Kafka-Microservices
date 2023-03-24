519cf7a86159872bdd9c02914aa73788c90b2d12
M. Mustafa Fındık <91599453+MustafaFindik24@users.noreply.github.com>
2023-03-24 17:17:08 +0300

    Delete src directory

--- src/main/java/com/mustafafindik/producerservice/dto/UserDto.java
+++
@@ -1,7 +0,0 @@
-package com.mustafafindik.producerservice.dto;
-
-import lombok.Data;
-@Data
-public class UserDto {
-    private String username;
-}
--- src/main/java/com/mustafafindik/producerservice/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.mustafafindik.producerservice.service;
-
-import model.entity.User;
-
-public interface UserService {
-    void createUser(User user);
-}
